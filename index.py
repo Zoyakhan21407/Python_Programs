afdf9f4881619cfea15b92b586824b4eeec2ae61
@Zoya_Khan <zoya21407khan@gmail.com>
2025-06-24 00:48:20 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+if i<10:
+print(“I is less then 10”)
+
+print("We can resolve the above error by declaring a variable of 'i' and assigning it a value of data type 'int'.")
+
+if no<0:
+print(“Positive no”)
+else:
+print(“Negative no”)
+
+print("We can resolve the above error by declaring a variable of 'no', assigning it a value of data type 'int' and by giving a one 'tab' space before print statement.")
+
+if (no<0) print(“Positive no”)
+else:
+print(“Negative no”)
+
+print("We can resolve the above error by declaring a variable of 'no', assigning it a value of data type 'int', moving the print statement to the next line and by giving a one 'tab' space before print statement.")
+
+else no<75:
+print(“you are fail”)
+
+print("We can resolve the above error by declaring a variable of 'no', assigning it a value of data type 'int', giving the 'if' condition and by giving a one 'tab' space before print statement.")
