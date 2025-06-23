f5392eae4c7ba5933fdfc7321d27df777f8b66b6
@Zoya_Khan <zoya21407khan@gmail.com>
2025-06-24 00:48:43 +0500

    Add files via upload

---
+++ word.py
@@ -0,0 +1,6 @@
+# Write a program that print every character of word in newline using ‘for loop’.
+
+word = input('Please enter a word to see its all characters in a new line: ')
+
+for words in word:
+    print(words)
